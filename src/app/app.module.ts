c89228cdd8a2f6f6a148a7610a14df48fb633f60
Yi-Jacob <jakeyi00@gmail.com>
2022-12-31 13:48:23 -0800

    finished directive practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClassDirective } from './class.directive';
+import { TimesDirective } from './times.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ClassDirective, TimesDirective],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
